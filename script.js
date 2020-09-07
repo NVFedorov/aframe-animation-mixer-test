59267101d2bbc7acf8d4e1ad16061c26f278f629
Nikita Fedorov <NFedorov@vubiquity.net>
2020-09-07 21:04:01 +0300

    switching AR and VR

--- script.js
+++ script.js
@@ -8,30 +8,12 @@ const box = {
 
 const entityModel = {
     url: "./assets/articuno/scene.gltf",
-    position: "0 0 -5",
+    position: "0 0 -10",
     rotation: "0 0 0",
     scale: "0.05 0.05 0.05"
 }
 
 const createScene = () => {
-    const scene = document.querySelector('a-scene');
-    scene.setAttribute('fog', 'type: linear; color: #AAA; far: 30; near: 0')
-    scene.setAttribute('background', 'color: #007FFF')
-
-    const camera = document.createElement('a-camera');
-    camera.setAttribute('rotation-reader', '');
-    const cursor = document.createElement('a-cursor');
-    cursor.setAttribute('color', '#FAFAFA');
-    camera.appendChild(cursor);
-    scene.appendChild(camera);
-
-    const plane = document.createElement('a-plane');
-    plane.setAttribute('position', '0 0 0');
-    plane.setAttribute('rotation', '-90 0 0');
-    plane.setAttribute('width', '100');
-    plane.setAttribute('height', '100');
-    plane.setAttribute('color', '#7BC8A4');
-    scene.appendChild(plane);
 }
 const createEntity = (model) => {
     let entity = document.createElement('a-entity');
@@ -66,4 +48,77 @@ function addEntity() {
         const scene = document.querySelector('a-scene');
         scene.appendChild(entity);
     }
+}
+
+let isArWorld = true;
+function AFrame(){
+    if (!isArWorld)
+        return;
+
+    isArWorld = false;
+    // clearInterval(interval);
+    const scene = document.querySelector('a-scene')
+    scene.removeAttribute('arjs-webcam-texture');
+    scene.setAttribute('fog', 'type: linear; color: #AAA; far: 30; near: 0')
+    // scene.setAttribute('animation__fog', 'property: fog.color; to: #000; dur: 2000')
+    scene.setAttribute('background', 'color: #007FFF')
+    const camera = document.querySelector('a-camera')
+    if (camera) {
+        camera.parentNode.removeChild(camera);
+        camera.destroy();
+    }
+    document.querySelectorAll('a-entity').forEach(e => {
+        e.parentNode.removeChild(e);
+        e.destroy();
+    });
+
+    const camera = document.createElement('a-camera');
+    camera.setAttribute('rotation-reader', '');
+    const cursor = document.createElement('a-cursor');
+    cursor.setAttribute('color', '#FAFAFA');
+    camera.appendChild(cursor);
+    scene.appendChild(camera);
+
+    const plane = document.createElement('a-plane');
+    plane.setAttribute('position', '0 0 0');
+    plane.setAttribute('rotation', '-90 0 0');
+    plane.setAttribute('width', '100');
+    plane.setAttribute('height', '100');
+    plane.setAttribute('color', '#7BC8A4');
+    scene.appendChild(plane);
+
+    scene.appendChild(createEntity(entityModel))
+    scene.render();
+}
+
+function ARjs(){
+    if (isArWorld)
+        return;
+
+    isArWorld = true;
+    const scene = document.querySelector('a-scene')
+    scene.removeAttribute('fog');
+    scene.removeAttribute('background');
+    scene.setAttribute('arjs-webcam-texture', '');
+    const camera = document.querySelector('a-camera')
+    if (camera) {
+        camera.parentNode.removeChild(camera);
+        camera.destroy();
+    }
+
+    document.querySelectorAll('a-entity').forEach(e => {
+        e.parentNode.removeChild(e);
+        e.destroy();
+    });
+    document.querySelectorAll('a-plane').forEach(e => {
+        e.parentNode.removeChild(e);
+        e.destroy();
+    });
+
+    const camera = document.createElement('a-camera');
+    camera.setAttribute('rotation-reader', '');
+    camera.setAttribute("gps-camera", "");
+    scene.appendChild(camera);
+    scene.appendChild(createEntity(entityModel));
+    scene.render();
 }
